1ca45683f72539775f563bc0297fb17020227a4a
MikeTeixeira <teixeiramichael5@gmail.com>
2017-12-28 20:32:52 -0500

    heroku settings configured

--- server/index.js
+++ server/index.js
@@ -7,6 +7,19 @@ app.get('/', (req,res) => {
   res.json({message: "hello"});
 });
 
+if (process.env.NODE_ENV === 'production') {
+
+  //Express serves up production assets -- main.js/main.css
+  app.use(express.static('client/build'));
+
+  //Express serves up index.html file if route not found
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendfile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+};
+
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);

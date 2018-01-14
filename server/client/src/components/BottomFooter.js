74a069b6cf5902f8c8776d29935ea4b1cec80040
MikeTeixeira <teixeiramichael5@gmail.com>
2018-01-14 17:10:37 -0500

    react-scroll

--- server/client/src/components/BottomFooter.js
+++ server/client/src/components/BottomFooter.js
@@ -1,28 +1,20 @@
 import React,{Component} from 'react';
-import { Footer } from 'react-materialize';
+import { Row,Col } from 'react-materialize';
 
 export default class BottomFooter extends Component {
 
   render(){
     return(
       <div className="footer-wrapper">
-        <Footer copyrights="&copy 2015 Copyright Text"
-          moreLinks={
-            <a className="grey-text text-lighten-4 right" href="#!">More Links</a>
-          }
-          links={
-            <ul>
-              <li><a className="grey-text text-lighten-3" href="#!">Link 1</a></li>
-              <li><a className="grey-text text-lighten-3" href="#!">Link 2</a></li>
-              <li><a className="grey-text text-lighten-3" href="#!">Link 3</a></li>
-              <li><a className="grey-text text-lighten-3" href="#!">Link 4</a></li>
-            </ul>
-          }
-          className='example'
-        >
-            <h5 className="white-text">Jamal Hamid</h5>
-            <p className="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
-        </Footer>
+        <Row className="footer-row" >
+          <Col className="footer-col1  footer-col "> Text</Col>
+          <Col className="footer-col2  footer-col"> Text</Col>
+          <Col className="footer-col3  footer-col"> Text</Col>
+        </Row>
+        <Row className="footer-row" >
+        <Col className="footer-col">Copyright © 2018 Jamal Hamid. All rights reserved. </Col>
+        <Col className="footer-col"> </Col>
+        </Row>
       </div>
     )
   }
